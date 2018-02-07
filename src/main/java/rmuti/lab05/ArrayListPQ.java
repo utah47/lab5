6e78ac89a03dd21f0d5f63ecb1ab31082215dd2b
utah47 <thanatchaithongpeng@gmail.com>
2018-02-07 15:37:42 +0700

    ttt

---
+++ src/main/java/rmuti/lab05/ArrayListPQ.java
@@ -0,0 +1,10 @@
+package rmuti.lab05;
+
+public class ArrayListPQ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/rmuti/lab05/TestArrayListPQ.java
@@ -0,0 +1,10 @@
+package rmuti.lab05;
+
+public class TestArrayListPQ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
